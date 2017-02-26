4bd2458c61de91a044756b5f5c3ef9880943d5e7
Narongchai Thongkongkeaw <socketgit@gmail.com>
2017-02-26 20:24:47 +0700

    update code

--- hookline.php
+++ hookline.php
@@ -4,7 +4,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 if (!is_null($events['ref'])) {
-    var_dump($events);
+    var_dump($events['ref']);
 }
 
 define('LINE_API',"https://notify-api.line.me/api/notify");
