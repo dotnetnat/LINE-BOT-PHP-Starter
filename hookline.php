1f680da9e80e286a9f250c5855b002a2a893ea3a
Narongchai Thongkongkeaw <socketgit@gmail.com>
2017-02-26 19:07:53 +0700

    update api

--- hookline.php
+++ hookline.php
@@ -6,4 +6,23 @@ $events = json_decode($content, true);
 if (!is_null($events['ref'])) {
     echo "Ref";
 }
-echo "OK";
+
+$data = [
+        'message' => 'ทดสอบ from Gogs',
+];
+$post = json_encode($data);
+$access_token = 'p1Htn19ebepP3RnHCyN4o4z9ngvyhZ7mmY4yLRRub5V';
+
+$url = 'https://notify-api.line.me/api/notify';
+
+$headers = array('Content-Type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
